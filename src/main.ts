5a407bc223e3411203c67fce1a257f61f0b17c81
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2024-01-15 17:18:36 +0100

    code cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +13,7 @@ async function bootstrap() {
     .setTitle('CinePik Catalog API')
     .setDescription('The CinePik Catalog microservice.')
     .setVersion(version)
-    .addBearerAuth()
+    // .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
