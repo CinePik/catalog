4d90829bd6af2b923aa1ca3567a41514be593353
Lan Zukanović <lzukanovic@gmail.com>
2024-01-17 14:02:19 +0100

    disable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: { origin: '*' } });
   const port = process.env.NODE_PORT;
   const version = process.env.npm_package_version;
 
