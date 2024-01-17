d24010f1b17441346a62789cdce78db1d0e43389
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2024-01-17 20:56:15 +0100

    code cleanup

--- src/common/common.controller.ts
+++ src/common/common.controller.ts
@@ -13,6 +13,12 @@ import { SearchResponseDto } from './dto/response/search-response.dto';
 
 @Controller('common')
 @ApiTags('common')
+@ApiInternalServerErrorResponse({
+  description: 'There was an error processing this request.',
+})
+@ApiBadRequestResponse({
+  description: 'Bad request.',
+})
 export class CommonController {
   constructor(
     private readonly commonService: CommonService,
@@ -29,12 +35,6 @@ export class CommonController {
     description:
       'The home endpoint offers a curated and dynamic homepage experience, delivering a rich selection of movies categorized to cater to diverse interests and preferences. Users can discover an array of films thoughtfully organized into genres, themes, and trending topics, ensuring they find the perfect cinematic experience tailored to their tastes.',
   })
-  @ApiInternalServerErrorResponse({
-    description: 'There was an error processing this request.',
-  })
-  @ApiBadRequestResponse({
-    description: 'Bad request.',
-  })
   home(): Promise<HomeResponseDto> {
     return this.metricsService.handleRequest(() => this.commonService.home());
   }
@@ -48,12 +48,6 @@ export class CommonController {
     summary: 'Search',
     description:
       'The Search endpoint empowers users to conduct customized searches, enabling them to discover movies and TV shows based on specific criteria such as genre, actor, director, or keyword, providing a tailored and efficient content discovery experience.',
-  })
-  @ApiInternalServerErrorResponse({
-    description: 'There was an error processing this request.',
-  })
-  @ApiBadRequestResponse({
-    description: 'Bad request.',
   })
   search(@Query('query') query: string): Promise<SearchResponseDto> {
     return this.metricsService.handleRequest(() =>
