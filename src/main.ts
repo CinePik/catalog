ba220850937ccb5fa7a6a5d2bbff9311ced21f35
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2024-01-18 19:16:42 +0100

    openapi url change

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     // .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('openapi', app, document);
 
   // Enable DTO validation
   app.useGlobalPipes(new ValidationPipe());
